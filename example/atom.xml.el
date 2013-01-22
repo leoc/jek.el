fed2970b6a87fe3c1bb3f6a1588104e0fc0c809e
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-01-22 16:37:52 +0100

    Remove xml tag from atom feed

--- example/atom.xml.el
+++ example/atom.xml.el
@@ -1,11 +1,10 @@
 (markup-xml
- (:xml
-  (:feed
-   (:title jekel-title)
-   (:link :href (concat jekel-url "/atom.xml") :rel "self")
-   (:link :href jekel-url)
-   (:updated "time")
-   (:id jekel-url)
-   (:author
-    (:name jekel-author)
-    (:email jekel-email)))))
+ (:feed
+  (:title jekel-title)
+  (:link :href (concat jekel-url "/atom.xml") :rel "self")
+  (:link :href jekel-url)
+  (:updated "time")
+  (:id jekel-url)
+  (:author
+   (:name jekel-author)
+   (:email jekel-email))))
